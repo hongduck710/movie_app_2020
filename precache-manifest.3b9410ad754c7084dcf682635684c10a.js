248ac9a57a8218240af56cdf7d37d66236d4ff65
hongduck710 <hongik710@gmail.com>
2020-07-13 13:54:15 +0900

    Updates

---
+++ precache-manifest.3b9410ad754c7084dcf682635684c10a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c192adf3772f01f63c33a3742cc2d0",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "09ab79723b93cacb5ba8",
+    "url": "/movie_app_2020/static/css/main.11f576f6.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ab79723b93cacb5ba8",
+    "url": "/movie_app_2020/static/js/main.f8fd3b1b.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
